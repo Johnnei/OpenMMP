47d9864be65be4181773ad6c73ac9e118b61173f
Johnnei Craft <johnnei07@live.nl>
2012-03-05 17:51:58 +0100

    Bye bye vector!

--- monopoly/src/gui/PlayerList.java
+++ monopoly/src/gui/PlayerList.java
@@ -1,20 +1,19 @@
 package gui;
 
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.util.ArrayList;
-import java.util.Vector;
-import javax.swing.JList;
-import monopoly.Game;
-import monopoly.Player;
 
-public class PlayerList extends JList
+import javax.swing.JComponent;
+import monopoly.Game;
+
+public class PlayerList extends JComponent
 {
 
-	private Game game;
-
-	public PlayerList(Game game, int WIDTH, int HEIGHT)
+	String[] playerList;
+	
+	public PlayerList(int WIDTH, int HEIGHT)
 	{
-		this.game = game;
 		setPreferredSize(new Dimension(WIDTH, HEIGHT));
 		setMinimumSize(new Dimension(WIDTH, HEIGHT));
 		setMaximumSize(new Dimension(WIDTH, HEIGHT));
@@ -24,15 +23,19 @@ public class PlayerList extends JList
 	public void doUpdate()
 	{
 		ArrayList<String> pList = new ArrayList<String>();
-		for(int i = 0; i < game.getPlayers().length; i++) {
-			if(game.getPlayer((byte)i) != null)
-				pList.add(game.getPlayer((byte)i).getUsername());
+		for(int i = 0; i < Game.getMonopoly().getPlayers().length; i++) {
+			if(Game.getMonopoly().getPlayer((byte)i) != null)
+				pList.add(Game.getMonopoly().getPlayer((byte)i).getUsername());
 		}
 		String[] plData = new String[pList.size()];
 		for(int i = 0; i < plData.length; i++) {
 			plData[i] = pList.get(i);
 		}
-		setListData(plData);
+		playerList = plData;
+	}
+	
+	public void paint(Graphics g) {
+		
 	}
 
 }
